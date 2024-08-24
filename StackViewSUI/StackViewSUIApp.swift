0f50b1470331623a6d471442ad9783a10c7d761c
Rafael <rafael.glez@gmail.com>
2024-08-23 20:33:53 -0600

    Initial Commit

---
+++ StackViewSUI/StackViewSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewSUIApp.swift
+//  StackViewSUI
+//
+//  Created by Rafael Gonzalez on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct StackViewSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
