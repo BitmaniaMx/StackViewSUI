51dcf0f2fe009f1b6f8ad4ce6b5ff4d1a00e7b06
Rafael <rafael.glez@gmail.com>
2024-08-25 18:06:03 -0600

    Ejemplo de uso de stack view utilizando SwiftUI

---
+++ StackViewSUI/FlagView.swift
@@ -0,0 +1,18 @@
+//
+//  FlagView.swift
+//  StackViewSUI
+//
+//  Created by Rafael Gonzalez on 24/08/24.
+//
+
+import SwiftUI
+
+struct FlagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlagView()
+}
